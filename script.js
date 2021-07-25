2a0817790e6905b25a23bd26bdab61a41d32fd3b
unknown <mikayel.h.y@tumo.org>
2021-07-26 02:00:56 +0400

    final

--- script.js
+++ script.js
@@ -2,91 +2,70 @@ const rabbCoords = []
 const homeCoords = []
 const wolfCoords = []
 const wallCoords = []
-const allCoords = []
-const all_blocks = []
+const rows = []
 
-var tableHeight = prompt("Input the table height")
-var tableWidth = prompt("Input the table width")
-var wolvesQuantity = prompt("Input the number of wolves")
-var wallsQuantity = prompt("Input the number of walls")
+// const tableHeight = prompt("Input the table height")
+// const tableWidth = prompt("Input the table width")
+// const wolvesQuantity = prompt("Input the number of wolves")
+// const wallsQuantity = prompt("Input the number of walls")
 
-var wolves = []
-var rabbit = document.createElement("div")
+let wolves = [], walls = []
+let rabbit = document.createElement("img"), finish = document.createElement("img")
 
-var body = document.getElementsByTagName('body')[0]
-
-document.write(body)
+const body = document.getElementsByTagName('body')[0]
 
 function createEmptyBoard(m, n){
     tbl = document.createElement('table')
-    tbl.style.width = '50%'
-    tbl.style.height = '50%'
     tbl.setAttribute('border', '1')
     tbdy = document.createElement('tbody')
     
     for (let i = 0; i < m; i++) {
         tr = document.createElement('tr')
-        all_blocks[i] = tr
-        all_blocks[i].length = n
+        rows[i] = tr
+        rows[i].length = n
         for (let j = 0; j < n; j++) {
             td = document.createElement('td')
             td.style.height = "40px"
             td.style.width = "40px"
             tr.appendChild(td)
-            all_blocks[i][j] = td
-            allCoords.push([i, j])
+            rows[i][j] = td
         }
         tbdy.appendChild(tr)
     }
     tbl.appendChild(tbdy)
     body.appendChild(tbl)
-    rows = document.getElementsByTagName('tbody')[0].rows
 }
 
-function createWolves(wolvesQuantity){
-    for(let i = 0; i < wolvesQuantity; i++){
-        var randWolves = Math.floor(Math.random() * allCoords.length)
-        var randWolvesY = allCoords[randWolves][0]
-        var randWolvesX = allCoords[randWolves][1]
-        wolfCoords.push([randWolvesY, randWolvesX])
-        allCoords.splice(randWolves, 1)
-        wolves[i] = document.createElement("div")
-        PaintCharachters(randWolvesY, randWolvesX, wolves[i], "🐺")
+const AddEachCharechter = (charY, charX, image, name, eachArr, tableHeight, tableWidth) => {
+    charY = Math.floor(Math.random() * tableHeight)
+    charX  = Math.floor(Math.random() * tableWidth)
+    if(rows[charY][charX].childNodes.length == 1) 
+        return AddEachCharechter(charY, charX, image, name, eachArr, tableHeight, tableWidth)
+    
+    eachArr.push(charY, charX)
+    PaintCharachters(charY, charX, image, name)
+}
+
+function createCharechters(num, image, arr, name, tableHeight, tableWidth){
+    let charX, charY
+
+    if(num > 1){
+        for(let i = 0; i < num; i++){
+            arr[i] = []
+            image[i] = document.createElement("img")
+            AddEachCharechter(charY, charX, image[i], name, arr[i], tableHeight, tableWidth)
+        }
+        return
     }
+    else if(num == 1) AddEachCharechter(charY, charX, image, name, arr, tableHeight, tableWidth)
 }
 
-function createWalls(wallsQuantity){
-    for(let i = 0; i < wallsQuantity; i++){
-        var randWalls = Math.floor(Math.random() * allCoords.length)
-        var randWallsY = allCoords[randWalls][0]
-        var randWallsX = allCoords[randWalls][1]
-        wallCoords.push([randWallsY, randWallsX])
-        rows[randWallsY][randWallsX].style.backgroundColor = "black"
-        allCoords.splice(randWalls, 1)
-    }
-}
-
-function createRabbit(arr){
-    var randRab = Math.floor(Math.random() * allCoords.length)
-    var rabY = allCoords[randRab][0]
-    var rabX = allCoords[randRab][1]
-    arr.push(rabY, rabX)
-    PaintCharachters(rabY, rabX, rabbit, "🐇")
-    allCoords.splice(randRab, 1)
-}
-
-function createHome(arr){
-    var randHome = Math.floor(Math.random() * allCoords.length)
-    var homeY = allCoords[randHome][0]
-    var homeX = allCoords[randHome][1]
-    arr.push(homeY, homeX)
-    rows[homeY][homeX].style.backgroundColor = "blue"
-    allCoords.splice(randHome, 1)
-}
-
-const PaintCharachters = (y, x, element, emoji) => {
-    element.appendChild(document.createTextNode(emoji))
-    rows[y][x].appendChild(element)
+const PaintCharachters = (y, x, image, name) => {
+    image.src = `./images/${name}.png`
+    image.setAttribute("height", "30px")
+    image.setAttribute("width", "30px")
+    image.classList.add(`${name}`)
+    rows[y][x].appendChild(image)
 }
 
 const checkWrongValues = (tableHeight, tableWidth, wolvesQuantity, wallsQuantity) => {
@@ -97,86 +76,95 @@ const checkWrongValues = (tableHeight, tableWidth, wolvesQuantity, wallsQuantity
     }
 }
 
-
 function startGame(tableHeight, tableWidth, wolvesQuantity, wallsQuantity) {
     checkWrongValues(tableHeight, tableWidth, wolvesQuantity, wallsQuantity);
     createEmptyBoard(tableHeight, tableWidth)
-    createWolves(wolvesQuantity)
-    createWalls(wallsQuantity)
-    createRabbit(rabbCoords)
-    createHome(homeCoords)
+    createCharechters(1, rabbit, rabbCoords, "rabbit", tableHeight, tableWidth)
+    createCharechters(1, finish, homeCoords, "finish", tableHeight, tableWidth)
+    createCharechters(wallsQuantity, walls, wallCoords, "wall", tableHeight, tableWidth)
+    createCharechters(wolvesQuantity, wolves, wolfCoords, "wolf", tableHeight, tableWidth)
 }
 
-startGame(tableHeight, tableWidth, wolvesQuantity, wallsQuantity)
+// startGame(tableHeight, tableWidth, wolvesQuantity, wallsQuantity)
+startGame(10, 10, 15, 10)
 
 
 window.addEventListener("keyup", event => moveRabbit(event.key))
 
 const moveRabbit = (key) => {
     if(key === "ArrowUp"){
-        if(checkRabbitCollision(1, 0, -(all_blocks.length - 1), 0)){}
-        else makeRabbitMove(0, -1, 0, all_blocks.length - 1)
+        if(checkRabbitCollision(1, 0, -(rows.length - 1), 0)){}
+        else makeRabbitMove(0, -1, 0, rows.length - 1)
     }
 
     else if(key === "ArrowDown"){
-        if(checkRabbitCollision(-1, 0, all_blocks.length - 1, 0)){}
-        else makeRabbitMove(0, 1, all_blocks.length - 1, 0)
+        if(checkRabbitCollision(-1, 0, rows.length - 1, 0)){}
+        else makeRabbitMove(0, 1, rows.length - 1, 0)
     }
 
     else if(key === "ArrowRight"){
-        if(checkRabbitCollision(0, -1, 0, all_blocks[0].length - 1)){}
-        else makeRabbitMove(1, 1, all_blocks[0].length - 1, 0)
+        if(checkRabbitCollision(0, -1, 0, rows[0].length - 1)){}
+        else makeRabbitMove(1, 1, rows[0].length - 1, 0)
     }
 
     else if(key === "ArrowLeft"){
-        if(checkRabbitCollision(0, 1, 0, -(all_blocks[0].length - 1))){}
-        else makeRabbitMove(1, -1, 0, all_blocks[0].length - 1)
+        if(checkRabbitCollision(0, 1, 0, -(rows[0].length - 1))){}
+        else makeRabbitMove(1, -1, 0, rows[0].length - 1)
     }
 
 
 }
 
-const makeRabbitMove = (rabbitCoordIndex, direction, tableEdge, value) => {
-    if(rabbCoords[rabbitCoordIndex] == tableEdge) rabbCoords[rabbitCoordIndex] = value
-    else rabbCoords[rabbitCoordIndex] += direction
+const makeRabbitMove = (index, direction, tableEdge, value) => {
+    if(rabbCoords[index] == tableEdge) rabbCoords[index] = value
+    else rabbCoords[index] += direction
     rows[rabbCoords[0]][rabbCoords[1]].appendChild(rabbit)
-    isWon()
-    moveWolf()
+    if(!isWon()) moveWolf()
 }
 
 const checkRabbitCollision = (coordY, coordX, edgeY, edgeX) => {
-    for(let i = 0; i < wallCoords.length; i++){
-        if((rabbCoords[0] == wallCoords[i][0] + coordY && rabbCoords[1] == wallCoords[i][1] + coordX) ||  
-        (rabbCoords[0] == wallCoords[i][0] + edgeY && rabbCoords[1] == wallCoords[i][1] + edgeX)) return true
-    }
+    const result = wallCoords.find(elem => (rabbCoords[0] == elem[0] + coordY && rabbCoords[1] == elem[1] + coordX) ||  
+        (rabbCoords[0] == elem[0] + edgeY && rabbCoords[1] == elem[1] + edgeX)) 
+    if(result) return true
+}
 
-    return false
+const moveWolf = () => {
+    if(isLost()) return
+    filterLegalMovesOnly()
+    isLost()
 }
 
 const isWon = () => {
     if(rabbCoords[0] == homeCoords[0] && rabbCoords[1] == homeCoords[1]){ 
         tbl.remove()
         setTimeout(() => alert("YOU WON"), 1)
+        return true
     }
 }
 
 const isLost = () => {
-    wolfCoords.forEach(coords => {
-        if((rabbCoords[0] == coords[0] && rabbCoords[1] == coords[1])){ 
-            tbl.remove()
-            setTimeout(() => alert("YOU LOST"), 1)
-        }
-    });
+    const result = wolfCoords.find(coords => (rabbCoords[0] == coords[0] && rabbCoords[1] == coords[1]))
+    if(result){ 
+        tbl.remove()
+        setTimeout(() => alert("YOU LOST"), 1)
+        return true
+    }
 }
 
-const CalculateDistnace = (wCoords, index) => {
-    var distance = []
+const CalculateDistance = (wCoords, index) => {
+    const distance = []
 
-    for(let i = 0; i < wCoords.length; i++){
-        distance.push(Math.sqrt(Math.pow(wCoords[i][0] - rabbCoords[0], 2) + Math.pow(wCoords[i][1] - rabbCoords[1], 2)))
-    }
+    wCoords.map(item => distance.push(Math.sqrt(Math.pow(item[0] - rabbCoords[0], 2) + Math.pow(item[1] - rabbCoords[1], 2))))
 
-    var wolfMinDistanceIndex = distance.indexOf(Math.min(...distance))
+    // let wolfMinDistanceIndex = distance.indexOf(Math.min(...distance))
+    // const wolfMinDistanceIndex = distance.reduce((lowest, next, index) => {
+    //     return next < distance[lowest] ? index : lowest }, 0)
+    let wolfMinDistanceIndex = 0
+    // for(let i = 1; i < distance.length; i++){
+    //     if(distance[i] < distance[wolfMinDistanceIndex]) wolfMinDistanceIndex = i
+    // }
+
+    distance.forEach((elem, i) => { if(elem < distance[wolfMinDistanceIndex]) wolfMinDistanceIndex = i })
 
     wolfCoords[index][0] = wCoords[wolfMinDistanceIndex][0]
     wolfCoords[index][1] = wCoords[wolfMinDistanceIndex][1] 
@@ -186,54 +174,43 @@ const CalculateDistnace = (wCoords, index) => {
 
 const checkWolfCollision = (wCoords, num) => {
     for(let j = 0; j < num; j++){
-        for(let k = 0; k < wCoords.length; k++){
-            if(num != wolfCoords.length) {
-                if(wCoords[k][0] == wolfCoords[j][0] && wCoords[k][1] == wolfCoords[j][1]){
-                    wCoords.splice(k, 1)
-                }
-            }
-            else break
+        if(num != wolfCoords.length) {
+            wCoords.forEach((elem, i) => { if(elem[0] == wolfCoords[j][0] && elem[1] == wolfCoords[j][1]) wCoords.splice(i, 1) })
         }
+        else break
     }
 }
 
-const moveWolf = () => {
-    isLost();
-    filterLegalMovesOnly();
-    isLost();
-}
 
-const getWolfAllMoves = (coords) => {
-    return [[coords[0] + 1, coords[1]], [coords[0] - 1, coords[1]], [coords[0], coords[1] + 1], [coords[0], coords[1] - 1]]
-}
+const getWolfAllMoves = coords => [[coords[0] + 1, coords[1]], [coords[0] - 1, coords[1]], [coords[0], coords[1] + 1], [coords[0], coords[1] - 1]]
 
 const filterLegalMovesOnly = () => {
-    var allPossibleCoords = []
-    var legalCoords
-    var num = 0
+    let allPossibleCoords
+    let legalCoords
+    let num = 0
 
     for(let i = 0; i < wolfCoords.length; i++){
-        allPossibleCoords.push(getWolfAllMoves(wolfCoords[i]))
+        allPossibleCoords = getWolfAllMoves(wolfCoords[i])
     
-        for(let k = 0; k < allPossibleCoords[i].length; k++){
+        for(let k = 0; k < allPossibleCoords.length; k++){
             for(let j = 0; j < wallCoords.length; j++){
-                if((allPossibleCoords[i][k][0] == wallCoords[j][0] && allPossibleCoords[i][k][1] == wallCoords[j][1]) || 
-                allPossibleCoords[i][k][0] < 0 || allPossibleCoords[i][k][0] > all_blocks.length - 1 || allPossibleCoords[i][k][1] < 0 || allPossibleCoords[i][k][1] > all_blocks[0].length - 1){
-                    allPossibleCoords[i].splice(k, 1)
+                if((allPossibleCoords[k][0] == wallCoords[j][0] && allPossibleCoords[k][1] == wallCoords[j][1]) || 
+                allPossibleCoords[k][0] < 0 || allPossibleCoords[k][0] > rows.length - 1 || allPossibleCoords[k][1] < 0 || allPossibleCoords[k][1] > rows[0].length - 1){
+                    allPossibleCoords.splice(k, 1)
                     k -= 1
                     break
                 }
             }
         }
 
-        checkWolfCollision(allPossibleCoords[i], num)
-
+        checkWolfCollision(allPossibleCoords, num)
+        
         num++
-
-        legalCoords = allPossibleCoords[i]
+        
+        legalCoords = allPossibleCoords
 
         if(legalCoords.length != 0){
-            CalculateDistnace(legalCoords, i)
+            CalculateDistance(legalCoords, i)
         }
     }
 }
